6b5a1c677d8b55b6603ec234f322ca4e1657bdb8
seungohjin <corrobolea@gmail.com>
2024-10-29 18:28:10 +0900

    update: 검색 UI 작업 및 기능 추가

--- app/src/main/java/com/mogun/newsapp/MainActivity.kt
+++ app/src/main/java/com/mogun/newsapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.mogun.newsapp
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mogun.newsapp.adapter.NewsAdapter
@@ -36,14 +39,84 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val newsService = retrofit.create(NewsService::class.java)
+
         newsAdapter = NewsAdapter()
         binding.newsRecyclerView.apply {
             layoutManager = LinearLayoutManager(this@MainActivity)
             adapter = newsAdapter
         }
 
-        val newsService = retrofit.create(NewsService::class.java)
-        newsService.mainFeed().enqueue(object : Callback<NewsRss> {
+        // 피드
+        binding.feedChip.setOnClickListener {
+            binding.chipGroup.clearCheck()  // 칩 그룹 check 해제
+            binding.feedChip.isChecked = true
+
+            newsService.mainFeed().submitList()
+        }
+
+        // 정치
+        binding.politicsChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.politicsChip.isChecked = true
+
+            newsService.politicsNews().submitList()
+        }
+
+        // 경제
+        binding.economyChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.economyChip.isChecked = true
+
+            newsService.economyNews().submitList()
+        }
+
+        // 사회
+        binding.socialChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.socialChip.isChecked = true
+
+            newsService.societyNews().submitList()
+        }
+
+        // 정보기술
+        binding.itChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.itChip.isChecked = true
+
+            newsService.itNews().submitList()
+        }
+
+        // 스포츠
+        binding.sportChip.setOnClickListener {
+            binding.chipGroup.clearCheck()
+            binding.sportChip.isChecked = true
+
+            newsService.sportNews().submitList()
+        }
+
+        // 특정 키패드 버튼 클릭 이벤트
+        binding.searchTextInputEditText.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+                binding.chipGroup.clearCheck()
+                binding.searchTextInputEditText.clearFocus()
+
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+
+                newsService.search(binding.searchTextInputEditText.text.toString()).submitList()
+
+                return@setOnEditorActionListener true
+            }
+
+            return@setOnEditorActionListener false
+        }
+
+        newsService.mainFeed().submitList()
+    }
+
+    private fun Call<NewsRss>.submitList() {
+        enqueue(object : Callback<NewsRss> {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity", "onResponse: ${response.body()?.channel?.items}")
 
